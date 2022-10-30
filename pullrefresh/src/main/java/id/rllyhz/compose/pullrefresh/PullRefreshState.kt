954cfa161a42117d5aba73a63bbcc217e9c6c1bc
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-31 03:08:12 +0700

    Created PullRefreshState class | A state holder for refresh state

---
+++ pullrefresh/src/main/java/id/rllyhz/compose/pullrefresh/PullRefreshState.kt
@@ -0,0 +1,46 @@
+package id.rllyhz.compose.pullrefresh
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.foundation.MutatePriority
+import androidx.compose.foundation.MutatorMutex
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class PullRefreshState(
+    isRefreshing: Boolean
+) {
+    private val _scrollableOffset = Animatable(0f)
+    private val mutatorMutex = MutatorMutex()
+
+    /**
+     * Whether this [PullRefreshState] is currently refreshing or not.
+     */
+    var isRefreshing: Boolean by mutableStateOf(isRefreshing)
+
+    /**
+     * Whether a pull/dragDown is currently in progress.
+     */
+    var isPullInProgress: Boolean by mutableStateOf(false)
+        internal set
+
+    /**
+     * The current offset for the scrollable, in pixels.
+     */
+    val scrollableOffset: Float get() = _scrollableOffset.value
+
+    internal suspend fun animateOffsetTo(offset: Float) {
+        mutatorMutex.mutate {
+            _scrollableOffset.animateTo(offset)
+        }
+    }
+
+    /**
+     * Dispatch scroll delta in pixels from touch events.
+     */
+    internal suspend fun dispatchScrollDelta(delta: Float) {
+        mutatorMutex.mutate(MutatePriority.UserInput) {
+            _scrollableOffset.snapTo(_scrollableOffset.value + delta)
+        }
+    }
+}
